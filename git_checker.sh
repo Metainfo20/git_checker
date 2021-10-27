7d77daf94d4c70daf8900ce1349d3241626f928b
k.kovalevskii <k.kovalevskii@brandquad.ru>
2021-10-27 13:06:23 +0300

    removes ifs, add case

--- git_checker.sh
+++ git_checker.sh
@@ -68,52 +68,46 @@ f_menu_main_next () {
     if [ $exitstatus = 0 ]
     then	
         echo "Yes.Exit status was $?"
-        if [[ "$SELECTED_MODE" = "1)" ]]
-        then
+        case "$SELECTED_MODE" in
+            "1)")
             git pull $MAIN $CURRENT_BRANCH2 > git_pull
             whiptail --textbox git_pull \
             --title "Git pull output. Use arrow, page, home & end keys. Tab toggle option" \
             --scrolltext  24 80
             rm git_pull
             f_menu_main
-        fi
-        if [[ "$SELECTED_MODE" = "2)" ]]
-        then
+            ;;
+            "2)")
             echo $CURRENT_BRANCH
             f_menu_main
-        fi
-        if [[ "$SELECTED_MODE" = "3)" ]]
-        then
+            ;;
+            "3)")
             git status > git_status
             whiptail --textbox git_status \
             --title "Git status output. Use arrow, page, home & end keys. Tab toggle option" \
             --scrolltext  24 80
             rm git_status
             f_menu_main
-        fi
-        if [[ "$SELECTED_MODE" = "4)" ]]
-        then
+            ;;
+            "4)")
             git checkout $SUBMAIN
             f_menu_main
-        fi
-        if [[ "$SELECTED_MODE" = "5)" ]]
-        then
+            ;;
+            "5)")
             git fetch > git_fetch
             whiptail --textbox git_fetch --title "Git Fetch output"  --scrolltext  24 80
             rm git_fetch
             f_menu_main
-        fi
-        if [[ "$SELECTED_MODE" = "6)" ]]
-        then
+            ;;
+            "6)")
             BRANCH_CHECKOUT=$(whiptail --title "Git Checkout $PREFIX" \
             --inputbox "Input branch digit" 10 60 "$PREFIX" \
             --ok-button "$YES" --cancel-button "$NO" \
             3>&1 1>&2 2>&3)
             git checkout $BRANCH_CHECKOUT
             f_menu_main
-        fi
-        if [[ "$SELECTED_MODE" = "7)" ]]
-        then
+            ;;
+            "7)")
             GIT_CMD=$(whiptail --title "Run Git Command" --inputbox "Input git command" 10 60 "git log" \
             --ok-button "$YES" --cancel-button "$NO" \
             3>&1 1>&2 2>&3)
@@ -121,19 +115,18 @@ f_menu_main_next () {
             whiptail --textbox git_cmd_output --title "$GIT_CMD output"  --scrolltext  24 80
             rm git_cmd_output
             f_menu_main
-        fi
-        if [[ "$SELECTED_MODE" = "8)" ]]
-        then
+            ;;
+            "8)")
             git checkout -
             f_menu_main
-        fi
-        if [[ "$SELECTED_MODE" = "9)" ]]
-        then
+            ;;
+            "9)")
             PREFIX=$(whiptail --title "Set prefix" --inputbox "Input new prefix" 10 60 "$PREFIX" \
             --ok-button "$YES" --cancel-button "$NO" \
             3>&1 1>&2 2>&3)
             f_menu_main
-        fi
+            ;;
+        esac
     else
         exit
     fi
